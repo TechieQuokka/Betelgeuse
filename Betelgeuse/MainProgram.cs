916056229b34b9c8fca646083e4a8e2e16ee5716
Sirius <Sirius@JARVIS>
2024-12-12 08:29:57 +0900

    초기 커밋.

---
+++ Betelgeuse/MainProgram.cs
@@ -0,0 +1,11 @@
+﻿namespace Betelgeuse
+{
+    public class MainProgram
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine("Hello");
+            return;
+        }
+    }
+}
