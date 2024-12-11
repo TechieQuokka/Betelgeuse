2de280b29b9222857555f656638fd22fb65cca46
Sirius <Sirius@JARVIS>
2024-12-12 08:29:57 +0900

    server lib 생성.

---
+++ AsynchronousServer/PipeServer.cs
@@ -0,0 +1,15 @@
+﻿namespace AsynchronousServer
+{
+    public class PipeServer
+    {
+        private readonly string _pipeName;
+        
+        public PipeServer (string pipeName)
+        {
+            this._pipeName = pipeName;
+            return;
+        }
+
+
+    }
+}
