532fb7b5a59e2c1cdb16a8c70a9e2db8be629088
Sirius <everestfesf@gmail.com>
2024-12-12 09:09:08 +0900

    Server 구현 일부

---
+++ AsynchronousServer/DataType/ConnectedClient.cs
@@ -0,0 +1,17 @@
+﻿using System.IO.Pipes;
+
+namespace AsynchronousServer.DataType
+{
+    public class ConnectedClient
+    {
+        public Guid Id { get; private set; }
+        public NamedPipeServerStream PipeStream { get; private set; }
+
+        public ConnectedClient (Guid id, NamedPipeServerStream pipeStream)
+        {
+            this.Id = id;
+            this.PipeStream = pipeStream;
+            return;
+        }
+    }
+}
--- AsynchronousServer/PipeServer.cs
+++ AsynchronousServer/PipeServer.cs
@@ -1,15 +1,25 @@
-﻿namespace AsynchronousServer
+﻿using System.Collections.Concurrent;
+using AsynchronousServer.DataType;
+
+namespace AsynchronousServer
 {
     public class PipeServer
     {
         private readonly string _pipeName;
-        
+        private readonly CancellationTokenSource _cancellationTokenSource;
+        private readonly ConcurrentDictionary<Guid, ConnectedClient> _connectedClients;
+
         public PipeServer (string pipeName)
         {
             this._pipeName = pipeName;
+            this._cancellationTokenSource = new CancellationTokenSource ();
+            this._connectedClients = new ConcurrentDictionary<Guid, ConnectedClient>();
             return;
         }
 
+        public async Task StartServerAsync()
+        {
 
+        }
     }
 }
